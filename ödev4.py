486f3abb5705eab5f600ebc5373b379d5adb4561
prosman844 <osmanelveren0@gmail.com>
2026-08-24 20:43:01 +0300

    Add files via upload

---
+++ ödev4.py
@@ -0,0 +1,17 @@
+dongu = True
+while dongu:
+    try:
+        sayi = int(input("sayı giriniz: "))
+        dongu = False
+    except ValueError:
+        print("sayı gir sayı metin değil")
+        dongu = True
+def fibonacci(sayi):
+    onceki_sayi = 0
+    fibonacci_sayi = 1
+    for i in range(sayi):
+        print(onceki_sayi)
+        onceki_sayi, fibonacci_sayi = fibonacci_sayi, fibonacci_sayi + onceki_sayi
+
+
+fibonacci(sayi)
